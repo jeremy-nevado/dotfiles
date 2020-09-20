0696e85ec18a95e475f50cf75e7f9817e39c574e
jeremy-nevado <jeremynevado@gmail.com>
2020-09-20 13:41:42 -0400

    Changed opacity of alacritty. Added anaconda script to my scripts. Updated zshrc to remove auto initialize anaconda.

--- personal_scripts/my-scripts.sh
+++ personal_scripts/my-scripts.sh
@@ -6,5 +6,14 @@ sede () {
     sed -E "$1"
 }
 
-xmodmap -e 'keycode 66 = Escape'
-xmodmap -e 'clear Lock'
+xmodmap ~/.Xmodmap
+xcape -e 'Control_L=Escape'
+
+anaconda () {
+    source ~/anaconda3/bin/activate
+}
+
+# xmodmap -e 'Control_L = Escape'
+# xmodmap -e 'keycode 66 = Control_L'
+# xmodmap -e 'keycode 255 = Caps_Lock'
+# xmodmap -e 'clear Lock'
